20c770084ac14315992646b887667d5e28f0805f
hextraza <sebastiancbenjamin@gmail.com>
2023-04-29 16:23:59 -0700

    Finish spawning crowd

--- crowd_spawner.gd
+++ crowd_spawner.gd
@@ -2,9 +2,20 @@ extends Node3D
 
 
 func _ready():
-	for child in self.get_children():
+	var shift = false
+	var children = self.get_children()
+	
+	for i in range(children.size()):			
 		var beak = load("res://beak.tscn").instantiate()
-		beak.global_position = child.global_position
+		beak.position = children[i].position
+		
+		if shift:
+			var pos = beak.position
+			beak.position.x += 0.5
+			
+		if i % 27 == 0:
+			shift = !shift
+			
 		add_child(beak)
 
 func _process(delta):
